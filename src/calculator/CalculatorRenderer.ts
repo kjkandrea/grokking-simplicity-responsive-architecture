2783d6bad2c3f7785479d5d606fb7d74494fffbe
kjkandrea <kjkandrea@gmail.com>
2022-09-29 14:49:29 +0900

    feat: 사칙연산 렌더러 추가

--- src/calculator/CalculatorRenderer.ts
+++ src/calculator/CalculatorRenderer.ts
@@ -1,3 +1,15 @@
+type NumberSentence =
+  | 'ADDITION'
+  | 'SUBTRACTION'
+  | 'MULTIPLICATION'
+  | 'DIVISION';
+const numberSentences = [
+  'ADDITION',
+  'SUBTRACTION',
+  'MULTIPLICATION',
+  'DIVISION',
+] as const;
+
 export class CalculatorRenderer {
   private readonly rootElement: HTMLElement;
 
@@ -7,7 +19,30 @@ export class CalculatorRenderer {
 
   render() {
     const wrapperElement = document.createElement('div');
-    wrapperElement.textContent = 'CalculatorRenderer';
+    const {ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION} =
+      this.generateResultElementMap();
+    wrapperElement.append(ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION);
     this.rootElement.append(wrapperElement);
+  }
+
+  private generateResultElementMap() {
+    // eslint-disable-next-line node/no-unsupported-features/es-builtins
+    return Object.fromEntries(
+      numberSentences.map(sentence => [
+        sentence,
+        this.generateResultElement(sentence),
+      ])
+    );
+  }
+
+  private generateResultElement(numberSentence: NumberSentence) {
+    const wrapperElement = document.createElement('dl');
+    const titleElement = document.createElement('dt');
+    const dataElement = document.createElement('dd');
+    wrapperElement.append(titleElement, dataElement);
+    titleElement.textContent = numberSentence;
+    dataElement.textContent = '9999'; // TODO
+
+    return wrapperElement;
   }
 }
